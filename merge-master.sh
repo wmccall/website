68660b47ec8f0494885890fe66dc04d7749697a9
William McCall <wnm.code@gmail.com>
2019-11-06 12:46:12 -0500

    Test build

--- merge-master.sh
+++ merge-master.sh
@@ -1,1 +1,19 @@
-echo 'committed to master'
+npm run build
+
+mkdir git-work
+cd git-work
+git clone git@github.com:wmccall/test-site
+
+cd test-site
+
+rm -rf -v !(".git"|".gitignore"|"CNAME") 
+
+cp -r ../build/* ./
+
+git add -A
+
+git commit -m "Deploy site"
+
+git remote add origin git@github.com:wmccall/test-site.git
+
+git push -u origin master
