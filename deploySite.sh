1065320c9306626fad8525de67ebceedd160de0d
William McCall <wnm.code@gmail.com>
2019-11-13 11:15:21 -0500

    Update deploySite.sh

--- deploySite.sh
+++ deploySite.sh
@@ -3,20 +3,15 @@ shopt -s extglob
 
 mkdir git-work
 cd git-work
+
 git clone git@github.com:wmccall/wmccall.github.io
 
 cd wmccall.github.io
-
 rm -rf !(".git"|".gitignore"|"CNAME")
-
 cp -r ../../build/* ./
 
-git add -A
-
-git commit -m "Deploy site"
-
+git commit -a -m "Deploy site"
 git remote add origin git@github.com:wmccall/wmccall.github.io.git
-
 git push -u origin master
 
 cd ../../
